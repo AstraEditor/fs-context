e8e0c7f2dee3e53ffd7e5bb47a6bc53feb786170
KOSHINO <2381068747@qq.com>
2026-04-02 20:46:53 +0800

    Add Cool title & Config

--- src/fs-context/structs/ext_config.ts
+++ src/fs-context/structs/ext_config.ts
@@ -1,9 +1,30 @@
 export default interface ExtConfig {
+    /**
+     * Extension ID, it will not display.
+     */
     id: string;
+    /**
+     * Extension Name, it will show to user.
+     */
     name: string;
+    /**
+     * Description, but it will not display in editor.
+     */
     description: string;
+    /**
+     * Verison.
+     */
     version: string;
-    platform: ["tw", "gandi","40code","02engine","clipcc","empty","gandi","zerocat","ae"] | string[];
+    /**
+     * Extension supported platforms.
+     */
+    platform: ["tw", "gandi", "40code", "02engine", "clipcc", "empty", "gandi", "zerocat", "ae"] | string[];
+    /**
+     * Who made this extension.
+     */
     author: string;
+    /**
+     * Display language of this extension.
+     */
     language: string;
 }
